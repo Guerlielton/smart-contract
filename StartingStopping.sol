f6ff9a1dd72c78d7068ae632c93aa1d6502ffa7b
thinkserver <guerlielton@gmail.com>
2022-08-01 22:01:06 -0300

    Add: Start and Stop smart contract example

---
+++ StartingStopping.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.1;
+
+
+contract StartStopContract {
+    address public owner;
+    bool public paused; 
+
+    constructor() {
+        owner = msg.sender;
+    }
+    function sendMoney () public payable {
+
+    }
+
+    function setPaused(bool _paused) public {
+        require(msg.sender == owner, "You are not the owner");
+        paused = _paused;
+    }
+
+    function withdrawAllMoney(address payable _to) public {
+        require(owner == msg.sender, "You are not the owner");
+        require(paused == false, "Contract Paused");
+        _to.transfer(address(this).balance);
+    }
+
+}
